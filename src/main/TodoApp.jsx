8f086c841ee46a5c3411da12ef8a848cd488b25f
smjdalma <smjdalma@penta.co.kr>
2025-04-10 10:48:55 +0900

    fix: useState 함수 재호출 수정

--- src/main/TodoApp.jsx
+++ src/main/TodoApp.jsx
@@ -31,7 +31,16 @@ const getFilteredItems = (todos, currentTab) => {
 export default function TodoApp() {
   const [currentTab, setCurrentTab] = useState(tabsInfo[0].label);
 
-  const [todos, setTodos] = useState(readTodosFromLocalStorage());
+  /** 잘못된 사용: useStates는 rerender 마다 아래 초기값이 아닌 업데이트값을 사용하도록 설계되어 있으나,
+   * rerender 시의 useState 선언때문에 아래의 함수호출 ( 혹은 Network 통신일 수도 있음!! )이 계속 사용된다
+   * 이걸 방지해 주기 위해서 함수를 호출한 값( func() )을 전달하는 것이 아니라,
+   *    한 단계 콜백함수( () => func() )로 감싸줘야 한다.
+   *
+   *  function() { return returnTodosFromLocalStorage(); } 형식으로 설정해두면
+   *  초기값이 필요하지 않다면 내부함수를 호출하지 않는다.
+   */
+  // const [todos, setTodos] = useState(readTodosFromLocalStorage());
+  const [todos, setTodos] = useState(() => readTodosFromLocalStorage());
 
   const filtered = getFilteredItems(todos, currentTab);
 
